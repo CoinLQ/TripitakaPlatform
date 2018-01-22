f2b07e4443418c6415e22c40760294eaa686ecdf
xianbu <xianbu@tripitaka>
2018-01-23 06:14:03 +0800

    修改数据库用户

--- utils/init.sh
+++ utils/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo -u postgres psql <<END
 drop database if exists tripitaka_platform;
-drop user if exists dzj;
+drop user if exists lqzj;
 END
 sudo -u postgres psql -f utils/setup_db.sql
 
--- utils/setup_db.sql
+++ utils/setup_db.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE tripitaka_platform;
-CREATE USER dzj WITH PASSWORD 'dzj';
-ALTER ROLE dzj SET client_encoding TO 'utf8';
-ALTER ROLE dzj SET default_transaction_isolation TO 'read committed';
-ALTER ROLE dzj SET timezone TO 'PRC';
-GRANT ALL PRIVILEGES ON DATABASE tripitaka_platform TO dzj;
+CREATE USER lqzj WITH PASSWORD 'lqdzjsql';
+ALTER ROLE lqzj SET client_encoding TO 'utf8';
+ALTER ROLE lqzj SET default_transaction_isolation TO 'read committed';
+ALTER ROLE lqzj SET timezone TO 'PRC';
+GRANT ALL PRIVILEGES ON DATABASE tripitaka_platform TO lqzj;
 \q

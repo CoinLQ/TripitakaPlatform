19d2dc67b5785f354d2d0761b1aceec4785f85c8
xianbu <xianbu@tripitaka>
2018-01-23 20:56:18 +0800

    查看根据文字校对修正的切分数据

--- sutradata/views.py
+++ sutradata/views.py
@@ -27,3 +27,15 @@ def page_verify_cut_list(request):
         'cut_confirm_count': r.cut_confirm_count,
         } for r in queryset]
     return JsonResponse({'page_list': page_list})
+
+def cutfixed_pages(request):
+    queryset = Page.objects.exclude(cut_updated_at__isnull=True).values_list('pid', 'cut_updated_at', named=True)
+    page_list = [{
+        'pid': r.pid,
+        'cut_updated_at': int(r.cut_updated_at.timestamp())
+        } for r in queryset]
+    return render(request, 'sutradata/cutfixed_pages.html', {'page_list': page_list})
+
+def cutfixed_page_detail(request, pid):
+    page = get_object_or_404(Page, pid=pid)
+    return render(request, 'sutradata/cutfixed_page_detail.html', {'page': page})

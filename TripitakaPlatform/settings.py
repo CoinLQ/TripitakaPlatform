0caf3227a6bdc2c44aa202d2e2ed1daab5396ce7
xianbu <xianbu@tripitaka>
2018-02-05 07:35:56 +0800

    fix

--- TripitakaPlatform/settings.py
+++ TripitakaPlatform/settings.py
@@ -55,10 +55,10 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'TripitakaPlatform.jwt_auth_middleware.AuthenticationMiddleware',
-#    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
 #    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
@@ -182,6 +182,8 @@ JWT_AUTH = {
     'JWT_AUTH_COOKIE': 'auth',
     'JWT_COOKIE_DOMAIN': 'lqdzj.cn',
 }
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ORIGIN_WHITELIST = (
     'lqdzj.cn',

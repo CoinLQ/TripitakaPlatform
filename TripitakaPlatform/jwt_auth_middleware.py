8a0ca683598698bde052c7aa743ac969dec927a5
xianbu <xianbu@tripitaka>
2018-02-13 06:45:47 +0800

    领取任务改状态

--- TripitakaPlatform/jwt_auth_middleware.py
+++ TripitakaPlatform/jwt_auth_middleware.py
@@ -29,7 +29,7 @@ class AuthenticationMiddleware(MiddlewareMixin):
             "'django.contrib.auth.middleware.AuthenticationMiddleware'."
         ) % ("_CLASSES" if settings.MIDDLEWARE is None else "")
         request.user = JWTAuth.get_user_from_request(request)
-        request.user = Staff.objects.get(username='admin')
+        #request.user = Staff.objects.get(username='admin')
 
 class JWTAuth:
     @classmethod

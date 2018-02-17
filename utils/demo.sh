251bc2e963897124a7228f8c2998239f053b80d8
xianbu <xianbu@tripitaka>
2018-02-17 14:27:11 +0800

    生成demo任务的脚本

---
+++ utils/demo.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+sudo -u postgres psql <<END
+drop database if exists tripitaka_platform;
+drop user if exists lqzj;
+END
+sudo -u postgres psql -f utils/setup_db.sql
+
+rm -f tdata/migrations/000*
+rm -f tasks/migrations/000*
+rm -f rect/migrations/000*
+./manage.py makemigrations tdata
+./manage.py makemigrations tasks
+./manage.py makemigrations rect
+cp rect/sql/*.py rect/migrations/.
+./manage.py migrate
+./manage.py import_tripitaka_list
+./manage.py import_cbeta_huayan60
+./manage.py init
+./manage.py initjudge
+./manage.py initpunct
+./manage.py runserver 0.0.0.0:8000
+

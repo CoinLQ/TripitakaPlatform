8af874585c12ac6512cbd4aabf495f89e73cdf6d
xianbu <dang.xia@qq.com>
2018-03-12 15:44:09 +0800

    fix

---
+++ tasks/utils/variant_map.py
@@ -0,0 +1,29 @@
+from tdata.models import Configuration
+
+class VariantMap(object):
+    def __init__(self):
+        self.variant_map = None
+
+    def load_variant_map(self, text):
+        if self.variant_map:
+            return
+        ch_set = set(text)
+        config = Configuration.objects.first()
+        variant_map = {}
+        for line in config.variant.split('\n'):
+            line = line.strip()
+            if not line:
+                continue
+            map_ch = line[0]
+            for ch in line[1:]:
+                if ch in ch_set:
+                    map_ch = ch
+                    break
+            for ch in line:
+                variant_map[ch] = map_ch
+        self.variant_map = variant_map
+
+    def replace_variant(self, text):
+        ch_lst = [self.variant_map.get(ch, ch) for ch in text]
+        new_text = ''.join(ch_lst)
+        return new_text

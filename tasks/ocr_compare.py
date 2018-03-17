52c1a65cdf1b722cb4c3e8ae44ed3b381ed561d5
xianbu <dang.xia@qq.com>
2018-03-17 15:49:13 +0800

    文字校对文本比较算法调整

--- tasks/ocr_compare.py
+++ tasks/ocr_compare.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from cdifflib import CSequenceMatcher
 SequenceMatcher=CSequenceMatcher
-import re
+import re, sys
 
 from tdata.models import Configuration
 from tasks.models import CorrectSeg
@@ -9,12 +9,81 @@ from tasks.common import clean_separators
 from tasks.utils.text_align import get_align_pos
 from tasks.utils.variant_map import VariantManager
 
+class SeparatorManager(object):
+
+    def extract_ocr_separators(self, text):
+        if text == '':
+            return []
+        separators = []
+        pos = 0
+        sep = ''
+        for c in text:
+            if c in 'pb\n':
+                sep += c
+            else:
+                if sep:
+                    separators.append( (pos, sep) )
+                    sep = ''
+                pos += 1
+        return separators
+
+    def __init__(self, text):
+        self.separators = self.extract_ocr_separators(text)
+        self.count = len(self.separators)
+        self.index = 0
+        self.get_separator_pos()
+
+    def get_separator_pos(self):
+        if self.index < self.count:
+            self.pos = self.separators[self.index][0]
+        else:
+            self.pos = sys.maxsize
+
+    def move_to_next_separator(self):
+        self.index += 1
+        self.get_separator_pos()
+
+    def merge_text_separator(self, text, start_pos, end_pos, text_tag):
+        '''
+        start_pos是text对应的起始index
+        end_pos是text对应的结束index
+        '''
+        if text_tag == 'equal':
+            text_tag = CorrectSeg.TAG_EQUAL
+        else:
+            text_tag = CorrectSeg.TAG_DIFF
+        cur_pos = start_pos
+        text_lst = []
+        while self.pos >= cur_pos and self.pos <= end_pos:
+            if self.pos > cur_pos:
+                s = text[cur_pos - start_pos : self.pos - start_pos]
+                text_lst.append({
+                    'tag': text_tag,
+                    'text': s,
+                })
+                cur_pos = self.pos
+            for c in self.separators[self.index][1]:
+                if c == '\n':
+                    tag = CorrectSeg.TAG_LINEFEED
+                else:
+                    tag = CorrectSeg.TAG_P
+                text_lst.append({
+                    'tag': tag,
+                    'text': c,
+                })
+            self.move_to_next_separator()
+        if cur_pos < end_pos:
+            s = text[cur_pos - start_pos : end_pos - start_pos]
+            text_lst.append({
+                'tag': text_tag,
+                'text': s,
+            })
+        return text_lst
+
 class OCRCompare(object):
     '''
     用于文字校对前的文本比对，OCR文本除文本外，只含有p或\n
     '''
-
-    variant_manager = None
 
     @classmethod
     def insert(cls, result_lst, t):
@@ -81,21 +150,6 @@ class OCRCompare(object):
         elif text == '\n':
             return CorrectSeg.TAG_LINEFEED
         return CorrectSeg.TAG_DIFF
-
-    @classmethod
-    def check_variant_equal(cls, text1, text2):
-        if len(text1) != len(text2):
-            return False
-        for i in range(len(text1)):
-            ch1 = text1[i]
-            ch2 = text2[i]
-            if ch1 == ch2:
-                continue
-            map_ch1 = cls.variant_manager.variant_map.get(ch1, ch1)
-            map_ch2 = cls.variant_manager.variant_map.get(ch2, ch2)
-            if map_ch1 != map_ch2:
-                return False
-        return True
 
     @classmethod
     def combine_equal_seg(cls, correctsegs):
@@ -176,11 +230,13 @@ class OCRCompare(object):
         用于文字校对前的文本比对
         text1是基础本；text2是要比对的版本。
         """
-        if not cls.variant_manager:
-            cls.variant_manager = VariantManager()
-            cls.variant_manager.load_variant_map()
+        variant_manager = VariantManager()
+        variant_manager.load_variant_map(text2)
         SEPARATORS_PATTERN = re.compile('[pb\n]')
         text1 = SEPARATORS_PATTERN.sub('', text1)
+        separator_manager = SeparatorManager(text2)
+        text2 = SEPARATORS_PATTERN.sub('', text2)
+        text1 = variant_manager.replace_variant(text1)
         correctsegs = []
         pos = 0
         page_no = 0
@@ -188,65 +244,99 @@ class OCRCompare(object):
         char_no = 0
         opcodes = SequenceMatcher(lambda x: x in 'pb\n', text1, text2, False).get_opcodes()
         for tag, i1, i2, j1, j2 in opcodes:
-            if ((i2-i1) - (j2-j1) > 30):
-               base_text = ''
-            else:
-                base_text = text1[i1:i2]
-            if tag == 'equal':
-                correctseg = CorrectSeg(tag=CorrectSeg.TAG_EQUAL, position=pos, \
-                text1='', text2=text1[i1:i2], selected_text=text1[i1:i2], \
-                page_no=page_no, line_no=line_no, char_no=char_no)
-                correctsegs.append(correctseg)
-                pos += (j2 - j1)
-                page_no, line_no, char_no = cls.count_page_line(text2[j1:j2], page_no, line_no, char_no)
-            elif tag == 'insert':
-                base_text = ''
-                result_lst = cls.split_text(text2[j1:j2])
-                for result in result_lst:
-                    tag = cls.get_tag(result)
-                    correctseg = CorrectSeg(tag=tag, position=pos, \
-                    text1='', text2=result, selected_text=None, \
-                    page_no=page_no, line_no=line_no, char_no=char_no)
-                    if result in 'pb\n':
-                        correctseg.selected_text = result
-                    correctsegs.append(correctseg)
-                    pos += len(result)
-                    page_no, line_no, char_no = cls.count_page_line(result, page_no, line_no, char_no)
-            elif tag == 'replace':
-                result_lst = cls.split_text(text2[j1:j2])
-                replace_not_inserted = True # 有一次replace
-                all_sep = all( [ result in 'pb\n' for result in result_lst ] )
-                if all_sep:
-                    correctseg = CorrectSeg(tag=CorrectSeg.TAG_DIFF, position=pos, \
-                    text1=base_text, text2='', selected_text=None, \
-                    page_no=page_no, line_no=line_no, char_no=char_no)
-                    replace_not_inserted = False
-                    correctsegs.append(correctseg)
-                for result in result_lst:
-                    if result not in 'pb\n' and replace_not_inserted:
-                        correctseg = CorrectSeg(tag=CorrectSeg.TAG_DIFF, position=pos, \
-                        text1=base_text, text2=result, selected_text=None, \
-                        page_no=page_no, line_no=line_no, char_no=char_no)
-                        replace_not_inserted = False
-                        if OCRCompare.check_variant_equal(base_text, result):
-                            correctseg.tag = CorrectSeg.TAG_EQUAL
-                            correctseg.selected_text = result
-                    else:
-                        tag = cls.get_tag(result)
-                        correctseg = CorrectSeg(tag=tag, position=pos, \
-                        text1='', text2=result, selected_text=None, \
-                        page_no=page_no, line_no=line_no, char_no=char_no)
-                        if result in 'pb\n':
-                            correctseg.selected_text = result
-                    correctsegs.append(correctseg)
-                    pos += len(result)
-                    page_no, line_no, char_no = cls.count_page_line(result, page_no, line_no, char_no)
-            elif tag == 'delete':
+            if tag == 'delete':
                 correctseg = CorrectSeg(tag=CorrectSeg.TAG_DIFF, position=pos, \
-                text1=base_text, text2='', selected_text=None, \
+                text1=text1[i1:i2], text2='', selected_text=None, \
                 page_no=page_no, line_no=line_no, char_no=char_no)
                 correctsegs.append(correctseg)
-        return cls.combine_equal_seg(correctsegs)
+                continue
+            base_index = i1
+            consumed_text_len = 0
+            text_lst = separator_manager.merge_text_separator(text2[j1:j2], j1, j2, tag)
+            for text_seg in text_lst:
+                text_tag = text_seg['tag']
+                result = text_seg['text']
+                result_len = len(result)
+                correctseg = CorrectSeg(tag=text_tag, position=pos, \
+                text1='', text2=result, selected_text=None, \
+                page_no=page_no, line_no=line_no, char_no=char_no)
+                if result in 'pb\n':
+                    correctseg.selected_text = result
+                    page_no, line_no, char_no = cls.count_page_line(result, page_no, line_no, char_no)
+                else:
+                    if tag == 'equal':
+                        correctseg.selected_text = result
+                    elif tag == 'replace':
+                        consumed_text_len += result_len
+                        if consumed_text_len == (j2 - j1):
+                            end_index = i2
+                        else:
+                            end_index = min(base_index + result_len, i2)
+                        correctseg.text1 = text1[base_index : end_index]
+                        base_index = end_index
+                    char_no += result_len
+                correctsegs.append(correctseg)
+                pos += result_len
+
+            # if tag == 'equal':
+            #     text_lst = separator_manager.merge_text_separator(text2[j1:j2], j1, j2, tag)
+            #     for text_seg in text_lst:
+            #         tag = text_seg['tag']
+            #         result = text_seg['text']
+            #         result_len = len(result)
+            #         correctseg = CorrectSeg(tag=tag, position=pos, \
+            #         text1='', text2=result, selected_text=None, \
+            #         page_no=page_no, line_no=line_no, char_no=char_no)
+            #         correctsegs.append(correctseg)
+            #         if result in 'pb\n':
+            #             correctseg.selected_text = result
+            #             page_no, line_no, char_no = cls.count_page_line(result, page_no, line_no, char_no)
+            #         else:
+            #             correctseg.selected_text = result
+            #             char_no += result_len
+            #         pos += result_len
+            # elif tag == 'insert':
+            #     text_lst = separator_manager.merge_text_separator(text2[j1:j2], j1, j2, tag)
+            #     for text_seg in text_lst:
+            #         tag = text_seg['tag']
+            #         result = text_seg['text']
+            #         result_len = len(result)
+            #         correctseg = CorrectSeg(tag=tag, position=pos, \
+            #         text1='', text2=result, selected_text=None, \
+            #         page_no=page_no, line_no=line_no, char_no=char_no)
+            #         if result in 'pb\n':
+            #             correctseg.selected_text = result
+            #             page_no, line_no, char_no = cls.count_page_line(result, page_no, line_no, char_no)
+            #         else:
+            #             char_no += result_len
+            #         correctsegs.append(correctseg)
+            #         pos += result_len
+            # elif tag == 'replace':
+            #     base_index = i1
+            #     text_lst = separator_manager.merge_text_separator(text2[j1:j2], j1, j2, tag)
+            #     consumed_text_len = 0
+            #     for text_seg in text_lst:
+            #         tag = text_seg['tag']
+            #         result = text_seg['text']
+            #         result_len = len(result)
+            #         correctseg = CorrectSeg(tag=tag, position=pos, \
+            #         text1='', text2=result, selected_text=None, \
+            #         page_no=page_no, line_no=line_no, char_no=char_no)
+            #         if result in 'pb\n':
+            #             correctseg.selected_text = result
+            #             page_no, line_no, char_no = cls.count_page_line(result, page_no, line_no, char_no)
+            #         else:
+            #             consumed_text_len += result_len
+            #             if consumed_text_len == (j2 - j1):
+            #                 end_index = i2
+            #             else:
+            #                 end_index = min(base_index + result_len, i2)
+            #             correctseg.text1 = text1[base_index : end_index]
+            #             base_index = end_index
+            #             char_no += result_len
+            #         correctsegs.append(correctseg)
+            #         pos += result_len
+        return correctsegs
 
     @classmethod
     def compare_reel(cls, text1, text2):

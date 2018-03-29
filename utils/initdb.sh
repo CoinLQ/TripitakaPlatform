2795bd9df1ee3d9f954d944c29deb635a29f5cab
xianbu <dang.xia@qq.com>
2018-03-29 10:06:18 +0800

    migrations加入代码的版本管理中，初始化的脚本不再删除migrations

--- utils/initdb.sh
+++ utils/initdb.sh
@@ -4,11 +4,4 @@ drop user if exists lqzj;
 END
 sudo -u postgres psql -f utils/setup_db.sql
 
-rm -f tdata/migrations/000*
-rm -f tasks/migrations/000*
-rm -f rect/migrations/000*
-./manage.py makemigrations tdata
-./manage.py makemigrations tasks
-./manage.py makemigrations rect
-cp rect/sql/*.py rect/migrations/.
 ./manage.py migrate

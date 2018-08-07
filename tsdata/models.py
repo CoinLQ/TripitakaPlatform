44539221c08cad658baa2bf45a909205f8eafa5c
quguocheng <717866228@qq.com>
2018-08-07 15:24:04 +0800

    add on_delete

--- tsdata/models.py
+++ tsdata/models.py
@@ -12,7 +12,7 @@ from jwt_auth.models import Staff
 
 
 class BaseData(models.Model):
-    creator = models.ForeignKey(Staff, verbose_name='创建人', blank=True, null=True)
+    creator = models.ForeignKey(Staff, verbose_name='创建人', blank=True, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(verbose_name='创建时间', auto_now_add=True)
     updater = models.ForeignKey(Staff, null=True, blank=True, on_delete=models.SET_NULL, related_name='updater')
     update_at = models.DateTimeField(auto_now=True, verbose_name='更新时间')

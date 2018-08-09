5c585b53ad844780f9c2085299bb912a7287a365
quguocheng <717866228@qq.com>
2018-08-09 16:32:03 +0800

    完成龙泉经导入

--- dmXadmin/urls.py
+++ dmXadmin/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from dmXadmin.data_api import *
+from dmXadmin.lqdata_api import *
 urlpatterns = [
     url(r'^import_tripitaka_from_excel$', ImportTripiFromExcel.as_view()),
+    url(r'^import_lqsutra_from_excel$', ImportLQSutraFromExcel.as_view()),
     url(r'^import_volume_from_excel$', ImportVolFromExcel.as_view()),
 ]
